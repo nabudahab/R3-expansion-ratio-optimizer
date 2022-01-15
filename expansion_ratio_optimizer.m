8f75fc9ac1ab35eb21de9a34fff9942ef74e652e
SpenceTroop <97715969+SpenceTroop@users.noreply.github.com>
2022-01-15 11:09:12 -0500

    Add files via upload

--- expansion_ratio_optimizer.m
+++ expansion_ratio_optimizer.m
@@ -1,18 +1,31 @@
-function [epsilon] = expansion_ratio_optimizer(atm_pressure, chamber_pressure, A_t, Cf_vac, diam)
-    t_diam = 2*sqrt(A_t)/pi;
+%
+% function [epsilon] = expansion_ratio_optimizer(atm_pressure, chamber_pressure, A_t, Cf_vac, diam)
+
+   atm_pressure = 7.5;
+   chamber_pressure = 250;
+   A_t = 4;
+   Cf_vac = 1.6;
+   max_diam = 10;
+   index = 1;
+
+   t_diam = 2*(sqrt(A_t/pi));
     
     arr_impulse = [];
     
-    for i = t_diam:0.001:diam
-        A_e = pi/4*i^2;
-        arr_impulse = [arr_impulse, total_impulse(atm_pressure, chamber_pressure, A_e, Cf_vac, A_t)];
+    for diam = t_diam:0.01:max_diam
+        arr_diam(index) = diam;
+        A_e(index) = pi*((diam/2)^2);
+        arr_impulse = [arr_impulse, total_impulse(atm_pressure, chamber_pressure, A_e(index), Cf_vac, A_t)];
+
+        index = index + 1;
     end
     
-    plot(t_diam:0.001:diam, arr_impulse)
-    
-    diam_array = t_diam:0.001:diam;
-    
-    [maximum, i] = max(arr_impulse);
-    
-    epsilon = maximum;
-end
+   max_impulse_index = find(max(arr_impulse));
+
+   max_A_e = A_e(max_impulse_index);
+
+   epsilon = max_A_e/A_t;
+
+    plot(t_diam:0.01:diam, arr_impulse)
+
+    disp(epsilon)
--- total_impulse.m
+++ total_impulse.m
@@ -1,15 +1,14 @@
-function [I] = total_impulse(atm_pressure, chamber_pressure, A_e, Cf_vac, A_t)   
+function [I] = total_impulse(atm_pressure, P_c, A_e, Cf_vac, A_t)   
     Cf = zeros(length(atm_pressure));
     Cf = Cf(1,:);
     F = Cf;
     
     for i = 1:1:length(atm_pressure)
-        Cf(i) = Cf_vac - atm_pressure(i)*A_e;
-        F(i) = Cf(i) * A_t * chamber_pressure;
+        Cf(i) = Cf_vac - (atm_pressure(i)*A_e)/(P_c*A_t);
+        F(i) = Cf(i) * A_t * P_c;
     end
     
-    %Assuming timestep of 0.001 seconds
-    dt = 0.001;
+    dt = 15; %Using a single atmopshere pressure
     I = 0;
     for i = F
         I = I + i*dt;
