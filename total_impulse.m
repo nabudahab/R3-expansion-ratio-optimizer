fc5df202ca36e503cf4361f3f3cdcf55d1ba5ba7
nnathanm <97645824+nnathanm@users.noreply.github.com>
2022-01-20 17:23:37 -0500

    Update total_impulse.m

--- total_impulse.m
+++ total_impulse.m
@@ -1,10 +1,11 @@
-function [I] = total_impulse(atm_pressure, P_c, A_e, Cf_vac, A_t)   
+function [I] = total_impulse(atm_pressure, P_c, P_e, A_e, A_t, gamma)   
+    epsilon = A_e/A_t;
     Cf = zeros(length(atm_pressure));
     Cf = Cf(1,:);
     F = Cf;
     
     for i = 1:1:length(atm_pressure)
-        Cf(i) = Cf_vac - (atm_pressure(i)*A_e)/(P_c*A_t);
+        Cf(i) = (2*gamma.^2/(gamma-1)*(2/(gamma+1)).^((gamma+1)/(gamma-1))*(1-(P_e/P_c).^((gamma-1)/gamma))).^(1/2) + (P_e-atm_pressure(i))./P_c.*epsilon;
         F(i) = Cf(i) * A_t * P_c;
     end
     
